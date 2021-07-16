d5bf9d8a680435bf9ae57a685fe7c22f210b156a
Peter Knipp <peteraknipp@gmail.com>
2021-07-15 20:19:59 -0400

    Change readFile from async to sync, and store return in array of pojos.

--- orderZips.js
+++ orderZips.js
@@ -1,17 +1,12 @@
 const fs = require("fs");
 
-fs.readFile("zips.csv", "utf8", (err, data) => {
-    if (err) console.log(err);
-//   console.log("THE CONTENTS ARE:");
-//   console.log(data);
-    let rows = data.split("\n");
-    const headers = rows[0].split(',');
-    rows = rows.slice(1).filter(row => !!row).map(row => {
-        const vals = row.split(',');
-        row = headers.reduce((pojo, header, index) => {
-            return {...pojo, [header]: vals[index]};
-        }, {});
-        return {...row, tuple: row.state + row.rate_area};
-    }).sort((a, b) => Number(a.zipcode) - Number(b.zipcode));
-    console.log(rows);
-});
+let zipsRows = fs.readFileSync("./zips.csv", {encoding:'utf8', flag:'r'}).split("\n");
+const headers = zipsRows[0].split(',');
+zipsRows = zipsRows.slice(1).filter(row => !!row).map(row => {
+    const vals = row.split(',');
+    row = headers.reduce((pojo, header, index) => {
+        return {...pojo, [header]: vals[index]};
+    }, {});
+    return {...row, tuple: row.state + row.rate_area};
+}).sort((a, b) => a.zipcode - b.zipcode);
+
