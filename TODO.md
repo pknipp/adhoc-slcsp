15210e4667b50761bcf330b30eca3a16ba70ad61
Peter Knipp <peteraknipp@gmail.com>
2021-07-15 21:01:41 -0400

    Sort plans.csv DRY-ly.

---
+++ TODO.md
@@ -0,0 +1,3 @@
+consider sorting zips and plans async, so that they can work in parallel
+prune columns of sorted tables, to save space
+eliminate slice steps, by a simple "if" statement in the array method.
--- orderZips.js
+++ orderZips.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 let zipsRows = fs.readFileSync("./zips.csv", {encoding:'utf8', flag:'r'}).split("\n");
-const headers = zipsRows[0].split(',');
+let headers = zipsRows[0].split(',');
 zipsRows = zipsRows.slice(1).filter(row => !!row).map(row => {
     const vals = row.split(',');
     row = headers.reduce((pojo, header, index) => {
@@ -10,3 +10,16 @@ zipsRows = zipsRows.slice(1).filter(row => !!row).map(row => {
     return {...row, tuple: row.state + row.rate_area};
 }).sort((a, b) => a.zipcode - b.zipcode);
 
+let plansRows = fs.readFileSync("./plans.csv", {encoding:'utf8', flag:'r'}).split("\n");
+headers = plansRows[0].split(',');
+metalHeaderIndex = headers.indexOf('metal_level');
+plansRows = plansRows.slice(1).reduce((rows, row) => {
+    const vals = row.split(',');
+    if (vals[metalHeaderIndex] === 'Silver') {
+        row = headers.reduce((pojo, header, index) => {
+            return {...pojo, [header]: vals[index]};
+        }, {});
+        rows = [...rows, {...row, tuple: row.state + row.rate_area}];
+    }
+    return rows;
+}, []).sort((a, b) => a.tuple < b.tuple ? -1 : a.tuple > b.tuple ? 1 : 0);
