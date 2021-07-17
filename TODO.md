9aae39a866560b7fa7f1d5244bae3860d57b6c9d
Peter Knipp <peteraknipp@gmail.com>
2021-07-17 14:54:49 -0400

    Calculate ALL rates for each zipcode (but never do I find more than one).

--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
 consider sorting zips and plans async, so that they can work in parallel
+conduct a test to ensure that multiple rate-ares for a zip code will yield desired results.
--- orderZips.js
+++ orderZips.js
@@ -5,7 +5,7 @@ const binarySearch = (array, target, key, iMin = 0, iMax = array.length - 1) => 
     let xMax = array[iMax][key];
     if (xMin === target) return iMin;
     if (xMax === target) return iMax;
-    if (iMax - iMin < 2) return NaN;
+    if (iMax - iMin < 2) return null;
     let iNext = Math.floor((iMin + iMax) / 2);
     let xNext = array[iNext][key];
     if (xNext > target) {
@@ -19,6 +19,7 @@ const binarySearch = (array, target, key, iMin = 0, iMax = array.length - 1) => 
 const findMatches = (array, target, key1, key2) => {
     const matches = new Set();
     let iMatch = binarySearch(array, target, key1);
+    if (iMatch === null) return [];
     matches.add(array[iMatch][key2]);
     let i = iMatch + 1;
     let n = 1;
@@ -33,7 +34,7 @@ const findMatches = (array, target, key1, key2) => {
         i--;
         n++;
     }
-    return [matches, n];
+    return [Array.from(matches), n];
 }
 
 let zipsRows = fs.readFileSync("./zips.csv", {encoding:'utf8', flag:'r'}).split("\n");
@@ -49,6 +50,8 @@ zipsRows = (Array.from(zipsRows)).map(row => {
     return {zipcode: vals[0], tuple: vals[1]};
 }).sort((a, b) => a.zipcode - b.zipcode);
 
+console.log("zipsRows.length = ", zipsRows.length);
+
 let plansRows = fs.readFileSync("./plans.csv", {encoding:'utf8', flag:'r'}).split("\n");
 
 plansRows = plansRows.reduce((set, row, index) => {
@@ -62,14 +65,18 @@ plansRows = (Array.from(plansRows)).map(row => {
     return {tuple: vals[0], rate: vals[1]};
 }).sort((a, b) => a.tuple < b.tuple ? -1 : a.tuple > b.tuple ? 1 : 0);
 
+console.log("plansRows.length = ", plansRows.length);
+
 let slcspRows = fs.readFileSync("./slcsp.csv", {encoding: 'utf8', flag: 'r'}).split("\n");
 headers = slcspRows[0].split(',');
 
 slcspRows = slcspRows.slice(1).filter(row => !!row).map(row => {
-    let zip = row.slice(0, -1);
-    let matches = findMatches(zipsRows, zip, 'zipcode', 'tuple');
-    // console.log(zip, matches);
-//     // if there are multiple tuples for that zip, break
+    let zipcode = row.slice(0, -1);
+    let [tuples, n1] = findMatches(zipsRows, zipcode, 'zipcode', 'tuple');
+//     // if there are multiple tuples for that zipcode, break
+    let [rates, n2] = findMatches(plansRows, tuples[0], 'tuple', 'rate');
+    console.log(zipcode, tuples[0], n1, n2, rates);
+
 //     // binary search for tuple in plansRows
 //     // assemble all elements in plansRows for that rateArea
 //     // put those elements into a set, to eliminate dupes
