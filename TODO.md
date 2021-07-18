065d026f67775380af26ed4ebb98a1ce2e625cab
Peter Knipp <peteraknipp@gmail.com>
2021-07-18 15:37:29 -0400

    Tighten up code.

--- TODO.md
+++ TODO.md
@@ -1,2 +1,1 @@
 consider sorting zips and plans async, so that they can work in parallel
-conduct a test to ensure that multiple rate-ares for a zip code will yield desired results.
--- orderZips.js
+++ orderZips.js
@@ -1,98 +1,109 @@
 const fs = require("fs");
+const process = require("process");
 
+// "key" is the name of array's property by which the array is sorted
 const binarySearch = (array, target, key, iMin = 0, iMax = array.length - 1) => {
-    let xMin = array[iMin][key];
-    let xMax = array[iMax][key];
+    let [xMin, xMax] = [array[iMin][key], array[iMax][key]];
     if (xMin === target) return iMin;
     if (xMax === target) return iMax;
-    if (iMax - iMin < 2) return null;
-    let iNext = Math.floor((iMin + iMax) / 2);
+    if (iMax - iMin < 2) return ''; // The target was not found.
+    let iNext = Math.floor((iMin + iMax) / 2); // divide and conquer
     let xNext = array[iNext][key];
     if (xNext > target) {
         iMax = iNext;
     } else {
         iMin = iNext;
     }
-    // console.log(iNext, array[iNext][key]);
-    return binarySearch(array, target, key, iMin, iMax);
+    return binarySearch(array, target, key, iMin, iMax);  // recursive call
 }
 
-const findMatches = (array, target, key1, key2) => {
-    const matches = new Set();
+// This finds ALL matches in an ordered array.
+const findMatch = (array, target, key1, key2) => {
+    const matches = new Set(); // use set to eliminate duplicates
     let iMatch = binarySearch(array, target, key1);
-    if (iMatch === null) return [];
-    matches.add(array[iMatch][key2]);
+    if (!iMatch) return ''; // target was not found
+    let x = array[iMatch][key2];
+    matches.add(key1 === 'zipcode' ? x : Number(x));
     let i = iMatch + 1;
-    let n = 1;
+    // include elements which have same #key1 but larger index.
     while (array[i][key1] === target) {
-        matches.add(array[i][key2]);
+        x = array[i][key2];
+        matches.add(key1 === 'zipcode' ? x : Number(x));
         i++;
-        n++;
     }
     i = iMatch - 1;
+    // include elements which have same #key1 but smaller index.
     while (array[i][key1] === target) {
-        matches.add(array[i][key2]);
+        x = array[i][key2];
+        matches.add(key1 === 'zipcode' ? x : Number(x));
         i--;
-        n++;
     }
-    let returnArray = Array.from(matches);
-    if (key2 === "rate") returnArray.sort((a, b) => Number(a) - Number(b));
-    return returnArray;
+    if (key1 === 'zipcode') {
+        // Ambiguity arises if a zipcode is in multiple rate-areas.
+        return matches.size === 1 ? Array.from(matches)[0] : '';
+    } else {
+        // Second lowest cost only exists if there are at least two different rates.
+        if (matches.size < 2) return '';
+        let [lowest, secondLowest] = [Infinity, Infinity];
+        matches.forEach(rate => {
+            if (rate < lowest) {
+                [lowest, secondLowest] = [rate, lowest];
+            } else if (rate < secondLowest) {
+                secondLowest = rate;
+            }
+        });
+        return secondLowest;
+    }
 }
 
-let zipsRows = fs.readFileSync("./zips.csv", {encoding:'utf8', flag:'r'}).split("\n");
+let zipsRows = fs.readFileSync("./zips.csv", {encoding:'utf8'}).split("\n");
+let plansRows = fs.readFileSync("./plans.csv", {encoding:'utf8'}).split("\n");
 
 zipsRows = zipsRows.reduce((set, row, index) => {
     const vals = row.split(',');
+    // Ignore zeroth- and empty rows
+    // Consolidate columns and use a set of strings to eliminate dupes.
     if (index && row) set.add([vals[0], vals[1] + vals[4]].join(","));
     return set;
 }, new Set());
 
+// Convert from set to array of pojos in order to allow sorting.
 zipsRows = (Array.from(zipsRows)).map(row => {
     const vals = row.split(',');
     return {zipcode: vals[0], tuple: vals[1]};
-}).sort((a, b) => a.zipcode - b.zipcode);
+}).sort((a, b) => a.zipcode - b.zipcode); // sorted array allows for quick lookup
 
-let data = zipsRows.map(row => row.zipcode + ',' + row.tuple);
-fs.writeFileSync("./zipsRows.txt", data.join("\n"));
-
-let plansRows = fs.readFileSync("./plans.csv", {encoding:'utf8', flag:'r'}).split("\n");
+// let data = zipsRows.map(row => Object.values(row).join(',')).join('\n');
+// fs.writeFileSync("./zipsRows.txt", data);
 
 plansRows = plansRows.reduce((set, row, index) => {
     const vals = row.split(',');
+    // Ignore zeroth- and empty rows, and filter on 'Silver'
+    // Consolidate columns and use a set of strings to eliminate dupes.
     if (index && row && vals[2] === 'Silver') set.add([vals[1] + vals[4], vals[3]].join(","));
     return set;
 }, new Set());
 
+// Convert from set to array of pojos in order to allow sorting.
 plansRows = (Array.from(plansRows)).map(row => {
     const vals = row.split(',');
     return {tuple: vals[0], rate: vals[1]};
-}).sort((a, b) => a.tuple < b.tuple ? -1 : a.tuple > b.tuple ? 1 : a.rate < b.rate ? -1 : a.rate > b.rate ? 1 : 0);
+// sorted array allows for quick lookup
+}).sort((a,b)=>a.tuple<b.tuple?-1:a.tuple>b.tuple?1:a.rate<b.rate?-1:a.rate>b.rate?1:0);
 
-data = plansRows.map(row => row.tuple + ',' + row.rate);
-fs.writeFileSync("./plansRows", data.join("\n"));
+// data = plansRows.map(row => Object.values(row).join(',')).join('\n');
+// fs.writeFileSync("./plansRows.txt", data);
 
 let slcspRows = fs.readFileSync("./slcsp.csv", {encoding: 'utf8', flag: 'r'}).split("\n");
-headers = slcspRows[0].split(',');
+let output = slcspRows[0] + "\n"; // header
 
-slcspRows = slcspRows.slice(1).filter(row => !!row).map((row, index) => {
-    // if (!index) {
-    let zipcode = row.slice(0, -1);
-    let tuples = findMatches(zipsRows, zipcode, 'zipcode', 'tuple');
-//     // if there are multiple tuples for that zipcode, break
-    let rates = findMatches(plansRows, tuples[0], 'tuple', 'rate');
-    if (!rates.length) {
-        console.log(zipcode, []);
-    } else if (rates.length === 1) {
-        console.log(zipcode, ',');
-    } else {
-        console.log(zipcode, rates[1]);
+slcspRows = slcspRows.map((row, index) => {
+    if (index && row) { // ignore header row and empty rows
+        let tuple = findMatch(zipsRows, row.slice(0, -1), 'zipcode', 'tuple'); // find rate-area
+        let secondLowestRate = tuple && findMatch(plansRows, tuple, 'tuple', 'rate');
+        output += row + (secondLowestRate && secondLowestRate.toFixed(2)) + "\n";
     }
-
-//     // binary search for tuple in plansRows
-//     // assemble all elements in plansRows for that rateArea
-//     // put those elements into a set, to eliminate dupes
-//     // if the set has a single element, break
-//     // loop over set, taking 2nd-smallest value
-    // }
 });
+
+fs.writeFileSync("./slcspOUT.csv", output); // write to file
+process.stdout.write(output); // write to terminal
